d17af9d759f43d313a301e1b7117716bc44bf0cb
SYKim <cleome94@naver.com>
2022-11-17 18:21:01 +0900

    내정보페이지,관리자페이지만들기

---
+++ admin/index.php
@@ -0,0 +1,25 @@
+<?php
+include "../inc/session.php";
+
+if($s_id != "admin"){
+    echo "
+        <script type=\"text/javascript\">
+            alert(\"관리자 로그인이 필요합니다.\");
+            location.href=\"http://localhost/project/admin/login/login.php\";
+        </script>
+    ";
+    exit;
+};
+?>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
